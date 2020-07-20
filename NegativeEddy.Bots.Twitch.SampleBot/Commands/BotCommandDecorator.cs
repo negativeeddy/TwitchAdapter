66cb376f8adb34ea83b14f2c28a7e57c6183ae5f
Ben Williams <ben@negativeeddy.com>
2020-07-20 16:36:07 -0500

    remove middle decorators

--- NegativeEddy.Bots.Twitch.SampleBot/Commands/BotCommandDecorator.cs
+++ NegativeEddy.Bots.Twitch.SampleBot/Commands/BotCommandDecorator.cs
@@ -10,7 +10,7 @@ namespace NegativeEddy.Bots.Twitch.SampleBot.Commands
 {
     public abstract class BotCommandDecorator : IBotCommand
     {
-        public IBotCommand Command { get; }
+        public IBotCommand Command { get; private set; }
         protected BotCommandDecorator(IBotCommand command)
         {
             Command = command;
@@ -30,6 +30,25 @@ namespace NegativeEddy.Bots.Twitch.SampleBot.Commands
                 return new[] { this };
             }
         }
+
+        /// <summary>
+        /// Removes a decorator from the decorator chain
+        /// </summary>
+        /// <param name="decorator">a refrence to the decorator to remove</param>
+        public void Remove(BotCommandDecorator decorator)
+        {
+            // just reparent the next decorator down the chain
+            if (Command == decorator)
+            {
+                Command = decorator.Command;
+            }
+            else if (Command is BotCommandDecorator childDecorator)
+            {
+                // pass the responsibility on down the chain
+                childDecorator.Remove(decorator);
+            }
+        }
+
         public abstract Task ExecuteAsync(ITurnContext context, IList<string> args);
     }
 }
