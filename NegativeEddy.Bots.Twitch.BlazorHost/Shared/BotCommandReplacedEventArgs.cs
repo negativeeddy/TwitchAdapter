8522bdf11e7ceb472d68e46bb89accfe44d20a3b
Ben Williams <ben@negativeeddy.com>
2020-07-20 17:09:57 -0500

    delete decorators

---
+++ NegativeEddy.Bots.Twitch.BlazorHost/Shared/BotCommandReplacedEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using NegativeEddy.Bots.Twitch.SampleBot.Commands;
+
+namespace NegativeEddy.Bots.Twitch.BlazorHost.Shared
+{
+    public class BotCommandReplacedEventArgs
+    {
+        public IBotCommand oldCmd { get; set; }
+        public IBotCommand newCmd { get; set; }
+    }
+}
